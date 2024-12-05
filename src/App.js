5b2f5353e2cb5d967fc5f64a86912da279dea51b
maksat200 <bakirovmaksat7@gmail.com>
2024-12-05 12:43:15 +0600

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button size = "sm">click</Button>
+      <Button size = "px">click</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import "./index.css";
+function Button(props) {
+    const {size = "md", variant = "primary", children} = props;
+    const className = `my-button my-button--${variant} my-button--${size}`;
+
+    return <button className={className}>{children}</button>
+}
+
+export default Button;
